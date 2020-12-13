38f42a253fc69a98abf6ee1be9ba02b368dc6271
anchaaay <ichautm14@gmail.com>
2020-12-13 16:57:07 +0700

    commit pertama

---
+++ database/migrations/2020_12_13_071116_create_absens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //onDelet cascade biar ngikut keapus pokoknya dahh
+            $table->foreignId('course_id')->constrained('course')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absens');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,13 @@ use App\Http\Controllers\API\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
+// middleware => 'auth' hanya untuk web
+
+Route::group(['middleware' => ['auth:sanctum']], function () { //berlaku pake token, sebagai penengah
     Route::get('/me', [AuthController::class, 'me']); //butuh token
     Route::post('/logout', [AuthController::class, 'logout']); //butuh token
-});
+
+    Route::post('/absen', [CourseController::class, 'absen']);
+
+    });
 
